4ef113d9ca041d09f12579868912b76f97cb0bd7
Erica Droz <ericad2891@gmail.com>
2018-05-24 19:43:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ function deleteFromObjectByKey(){
 return recipe
 }
 function destructivelyDeleteFromObjectByKey(recipes, FirstIngredient){
-  var recipe1= {FirstIngredient: "milk"}=recipes
   delete recipe1.FirstIngredient
   return recipes
 }
